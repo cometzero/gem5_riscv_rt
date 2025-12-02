e8431f35f81fd77801df8236d7005b98a247206b
Chanho Park <parkch98@gmail.com>
2025-12-02 19:25:57 +0900

    fix(scripts): set CONFIG_RV_BOOT_HART for AMP kernels

--- build_amp.sh
+++ build_amp.sh
@@ -9,10 +9,12 @@ cd ${APP_DIR}
 
 for i in {0..3}
 do
+    echo "CONFIG_RV_BOOT_HART=${i}" > gem5_amp_core${i}.conf
     echo "Building AMP Kernel for Core ${i}..."
     west build -b hifive1 -d build_core${i} --pristine -- \
-        -DEXTRA_CONF_FILE=gem5_amp.conf \
+        -DEXTRA_CONF_FILE="gem5_amp.conf;gem5_amp_core${i}.conf" \
         -DEXTRA_DTC_OVERLAY_FILE=gem5_amp_core${i}.overlay
+    rm gem5_amp_core${i}.conf
     echo "Core ${i} build complete."
 done
 
